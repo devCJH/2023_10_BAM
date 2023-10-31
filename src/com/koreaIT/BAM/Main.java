de91c07989937b1cfe86cd34be477e34d33aca6b
devCJH <http-@naver.com>
2023-10-31 20:54:50 +0900

    article list, article write, else, break분리

--- src/com/koreaIT/BAM/Main.java
+++ src/com/koreaIT/BAM/Main.java
@@ -8,13 +8,34 @@ public class Main {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		int lastArticleId = 0;
+		
 		while(true) {
-			String cmd = sc.nextLine();
 			
-			System.out.println("명령어) " + cmd);
+			System.out.printf("명령어) ");
+			
+			String cmd = sc.nextLine();
 			
 			if(cmd.equals("exit")) {
 				break;
+			}
+			
+			if (cmd.equals("article list")) {
+				
+				System.out.println("게시글이 없습니다");
+				
+			} else if (cmd.equals("article write")) {
+				lastArticleId++;
+				
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+				
+				System.out.println(lastArticleId + "번글이 생성되었습니다");
+				
+			} else {
+				System.out.println("존재하지 않는 명령어 입니다");
 			}
 		}
 		
