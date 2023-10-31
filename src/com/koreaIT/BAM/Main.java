fd03b643dcbfa55bdad5589004bd3c48c285cd57
devCJH <http-@naver.com>
2023-10-31 21:48:36 +0900

    trim(), Article, ArrayList, article list

--- src/com/koreaIT/BAM/Main.java
+++ src/com/koreaIT/BAM/Main.java
@@ -1,5 +1,7 @@
 package com.koreaIT.BAM;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -7,6 +9,8 @@ public class Main {
 		System.out.println("== 프로그램 시작 ==");
 		
 		Scanner sc = new Scanner(System.in);
+
+		List<Article> articles = new ArrayList<>();
 		
 		int lastArticleId = 0;
 		
@@ -14,17 +18,35 @@ public class Main {
 			
 			System.out.printf("명령어) ");
 			
-			String cmd = sc.nextLine();
+			String cmd = sc.nextLine().trim();
 			
+			if (cmd.length() == 0) {
+				System.out.println("명령어를 입력해주세요");
+				continue;
+			}
+
 			if(cmd.equals("exit")) {
 				break;
 			}
 			
 			if (cmd.equals("article list")) {
 				
-				System.out.println("게시글이 없습니다");
+				if (articles.size() == 0) {
+					System.out.println("게시글이 없습니다");
+					continue;
+				}
+				
+				System.out.println("== article list ==");
+				System.out.println("번호	|	제목");
+				
+				for (int i = articles.size() - 1; i >= 0; i--) {
+					Article article = articles.get(i);
+					
+					System.out.printf("%d	|	%s\n", article.id, article.title);
+				}
 				
 			} else if (cmd.equals("article write")) {
+				System.out.println("== article write ==");
 				lastArticleId++;
 				
 				System.out.printf("제목 : ");
@@ -32,15 +54,32 @@ public class Main {
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
 				
+				Article article = new Article(lastArticleId, title, body);
+				
+				articles.add(article);
+				
 				System.out.println(lastArticleId + "번글이 생성되었습니다");
 				
 			} else {
 				System.out.println("존재하지 않는 명령어 입니다");
 			}
+			
 		}
 		
 		sc.close();
 
 		System.out.println("== 프로그램 종료 ==");
+	}
+}
+
+class Article {
+	int id;
+	String title;
+	String body;
+	
+	Article(int id, String title, String body){
+		this.id = id;
+		this.title = title;
+		this.body = body;
 	}
 }
