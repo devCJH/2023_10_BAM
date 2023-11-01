8f1be3a8b7539527eed8f61c06dca954dd06ca99
devCJH <http-@naver.com>
2023-11-01 19:37:34 +0900

    article detail 진행중

--- src/com/koreaIT/BAM/Main.java
+++ src/com/koreaIT/BAM/Main.java
@@ -7,65 +7,69 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("== 프로그램 시작 ==");
-		
+
 		Scanner sc = new Scanner(System.in);
 
 		List<Article> articles = new ArrayList<>();
-		
+
 		int lastArticleId = 0;
-		
-		while(true) {
-			
+
+		while (true) {
+
 			System.out.printf("명령어) ");
-			
+
 			String cmd = sc.nextLine().trim();
-			
+
 			if (cmd.length() == 0) {
 				System.out.println("명령어를 입력해주세요");
 				continue;
 			}
 
-			if(cmd.equals("exit")) {
+			if (cmd.equals("exit")) {
 				break;
 			}
-			
-			if (cmd.equals("article list")) {
-				
-				if (articles.size() == 0) {
-					System.out.println("게시글이 없습니다");
-					continue;
-				}
-				
-				System.out.println("== article list ==");
-				System.out.println("번호	|	제목");
-				
-				for (int i = articles.size() - 1; i >= 0; i--) {
-					Article article = articles.get(i);
-					
-					System.out.printf("%d	|	%s\n", article.id, article.title);
-				}
-				
-			} else if (cmd.equals("article write")) {
+			if (cmd.equals("article write")) {
 				System.out.println("== article write ==");
 				lastArticleId++;
-				
+
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
-				
+
 				Article article = new Article(lastArticleId, title, body);
-				
+
 				articles.add(article);
-				
+
 				System.out.println(lastArticleId + "번글이 생성되었습니다");
-				
+
+			} else if (cmd.equals("article list")) {
+
+				if (articles.size() == 0) {
+					System.out.println("게시글이 없습니다");
+					continue;
+				}
+
+				System.out.println("== article list ==");
+				System.out.println("번호	|	제목");
+
+				for (int i = articles.size() - 1; i >= 0; i--) {
+					Article article = articles.get(i);
+
+					System.out.printf("%d	|	%s\n", article.id, article.title);
+				}
+
+			} else if (cmd.startsWith("article detail ")) {
+
+				String[] cmdBits = cmd.split(" ");
+				int id = Integer.parseInt(cmdBits[2]);
+
 			} else {
 				System.out.println("존재하지 않는 명령어 입니다");
 			}
-			
+
 		}
-		
+
 		sc.close();
 
 		System.out.println("== 프로그램 종료 ==");
@@ -76,8 +80,8 @@ class Article {
 	int id;
 	String title;
 	String body;
-	
-	Article(int id, String title, String body){
+
+	Article(int id, String title, String body) {
 		this.id = id;
 		this.title = title;
 		this.body = body;
