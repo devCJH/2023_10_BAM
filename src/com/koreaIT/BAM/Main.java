28de59e615516d0eec467950df145ea56e0e7d69
devCJH <http-@naver.com>
2023-11-02 19:47:53 +0900

    Util.getDate()

--- src/com/koreaIT/BAM/Main.java
+++ src/com/koreaIT/BAM/Main.java
@@ -31,13 +31,13 @@ public class Main {
 			if (cmd.equals("article write")) {
 				System.out.println("== article write ==");
 				lastArticleId++;
-
+				String regDate = Util.getDate();
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
 
-				Article article = new Article(lastArticleId, title, body);
+				Article article = new Article(lastArticleId, regDate, title, body);
 
 				articles.add(article);
 
@@ -51,12 +51,12 @@ public class Main {
 				}
 
 				System.out.println("== article list ==");
-				System.out.println("번호	|	제목");
+				System.out.println("번호	|		날짜		|	제목");
 
 				for (int i = articles.size() - 1; i >= 0; i--) {
 					Article article = articles.get(i);
 
-					System.out.printf("%d	|	%s\n", article.id, article.title);
+					System.out.printf("%d	|	%s	|	%s\n", article.id, article.regDate, article.title);
 				}
 
 			} else if (cmd.startsWith("article detail ")) {
@@ -82,22 +82,23 @@ public class Main {
 				
 				System.out.printf("== %d번 게시물 상세보기 ==\n", foundArticle.id);
 				System.out.printf("번호 : %d\n", foundArticle.id);
+				System.out.printf("작성일 : %s\n", foundArticle.regDate);
 				System.out.printf("제목 : %s\n", foundArticle.title);
 				System.out.printf("내용 : %s\n", foundArticle.body);
 				
 			} else if (cmd.startsWith("article delete ")) {
-
+				
+				System.out.println("== article delete ==");
+				
 				String[] cmdBits = cmd.split(" ");
 				int id = Integer.parseInt(cmdBits[2]);
 
-//				Article foundArticle = null;
 				int foundIndex = -1;
 				
 				for (int i = 0; i < articles.size(); i++) {
 					Article article = articles.get(i);
 					
 					if (article.id == id) {
-//						foundArticle = article;
 						foundIndex = i;
 						break;
 					}
@@ -108,7 +109,6 @@ public class Main {
 					continue;
 				}
 				
-//				articles.remove(foundArticle);
 				articles.remove(foundIndex);
 				
 				System.out.printf("%d번 게시물이 삭제되었습니다\n", id);
@@ -127,11 +127,13 @@ public class Main {
 
 class Article {
 	int id;
+	String regDate;
 	String title;
 	String body;
 
-	Article(int id, String title, String body) {
+	Article(int id, String regDate, String title, String body) {
 		this.id = id;
+		this.regDate = regDate;
 		this.title = title;
 		this.body = body;
 	}
